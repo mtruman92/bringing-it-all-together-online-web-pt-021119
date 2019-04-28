c61226c845338c9a71dddc5ab53dc7e1d47df6b8
Mickae Truman <mtruman@nyit.edu>
2019-04-28 19:27:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,4 +41,10 @@ class Dog
     dog.save 
     dog
   end
+  
+  def self.find_by_id 
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0])
+  end
 end

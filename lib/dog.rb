2e74670b831e6ef7e89a10a6889edd483ae2a9a5
Mickae Truman <mtruman@nyit.edu>
2019-04-28 19:54:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ end
  end
  
  def update 
-   sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+   sql = "UPDATE dogs SET name = ?, breed = ? WHERE ID = ?"
    DB[:conn].execute(sql, self.name, self.breed, self.id)
  end
 end

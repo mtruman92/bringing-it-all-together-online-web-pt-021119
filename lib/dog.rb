800d11791b33518e6195c6a5636c6eab0e56ee32
Mickae Truman <mtruman@nyit.edu>
2019-04-28 19:51:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,4 +62,12 @@ end
    dog = DB[:conn].execute(sql,name,breed)
    
  end
+ 
+ def self.new_from_db(row)
+   id = row[0]
+   name = row[1]
+   breed = row[2]
+   new_dog = self.new(id: id, name: name, breed: breed)
+   new_dog
+ end
 end
